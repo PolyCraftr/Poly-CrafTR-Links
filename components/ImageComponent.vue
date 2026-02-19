5f21a0d2da202c0b0d7395b51af79b9f87d6ea98
altan aydin <altanaydin1359@gmail.com>
2026-02-19 23:10:02 +0300

    Dinamik resim yollari düzeltildi

--- components/ImageComponent.vue
+++ components/ImageComponent.vue
@@ -1,18 +1,17 @@
 <template>
   <div @mouseleave="flipped = !flipped" @mouseenter="flipped = !flipped" class="flip-container w-28 h-28">
     <div :class="['flipper', flipped ? 'flipped' : '']">
-      <!-- Front Image -->
       <div class="front">
         <img
           class="w-28 h-28 object-cover rounded-full"
-          :src="props.frontImage"
+          :src="getImageUrl(props.frontImage)"
           alt="User Image"
         />
       </div>
       <div class="back">
         <img
           class="w-28 h-28 object-cover rounded-full"
-          :src="props.backImage"
+          :src="getImageUrl(props.backImage)"
           alt="Back Image"
         />
       </div>
@@ -23,13 +22,18 @@
 <script setup>
 import { ref } from 'vue';
 
-// Dışarıdan alacağımız resim verilerini tanımlıyoruz
 const props = defineProps({
   frontImage: String,
   backImage: String
 });
 
 const flipped = ref(false);
+
+const config = useRuntimeConfig();
+
+const getImageUrl = (imageName) => {
+  return config.app.baseURL + imageName;
+};
 </script>
 
 <style scoped>
